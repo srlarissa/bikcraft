843c06cdf9a53fda9aa32cca92ca960b1903f093
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-04-01 21:44:56 -0300

    feat: php para recepção de posts

--- wp-content/themes/bikcraft/index.php
+++ wp-content/themes/bikcraft/index.php
@@ -1,7 +1,23 @@
 
-<?php	get_header();	?>
-	<p>Página Index</p>
-<?php	get_footer();	?>
+<?php	
+	get_header();
+	if ( have_posts() ) : while ( have_posts() ) : the_post();
+?>
+
+
+		<h1><?php	the_title();	?></h1>
+<?php	
+	the_content();
+	endwhile; 
+	else:
+?>
+
+<p><?php	_e('Sorry, no posts matched your criteria.')	?></p>
+
+<?php	
+	endif;
+	get_footer();	
+?>
 
 	
 
