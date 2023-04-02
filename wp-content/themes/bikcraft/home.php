d59fc0e20cedcabfc2ab46c8c9ca9b2753c8119a
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-04-01 23:35:57 -0300

    fix: Portfolio adicionado via Snipet

--- wp-content/themes/bikcraft/home.php
+++ wp-content/themes/bikcraft/home.php
@@ -56,11 +56,7 @@
 		<section class="portfolio">
 			<div class="container">
 				<h2 class="subtitulo">Portfólio</h2>
-				<div class="portfolio_lista">
-					<div class="grid-8"><img src="<?php	echo get_template_directory_uri();	?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-					<div class="grid-8"><img src="<?php	echo get_template_directory_uri();	?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-					<div class="grid-16"><img src="<?php	echo get_template_directory_uri();	?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-				</div>
+				<?php	include(TEMPLATEPATH . "/inc/clientes-portfolio.php");	?>
 				<div class="call">
 					<p><?php	the_field('chamada_portfolio');	?></p>
 					<a href="/bikcraft/portfolio/" class="btn">Portfólio</a>
--- wp-content/themes/bikcraft/inc/clientes-portfolio.php
+++ wp-content/themes/bikcraft/inc/clientes-portfolio.php
@@ -2,14 +2,14 @@
 	<div class="container">
 		<ul class="portfolio_lista rslides_portfolio">
 			<li>
-				<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-				<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-				<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
+				<div class="grid-8"><img src="<?php echo get_template_directory_uri();	?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
+				<div class="grid-8"><img src="<?php echo get_template_directory_uri();	?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
+				<div class="grid-16"><img src="<?php echo get_template_directory_uri();	?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
 			</li>
 			<li>
-				<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-				<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-				<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
+				<div class="grid-8"><img src="<?php echo get_template_directory_uri();	?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
+				<div class="grid-8"><img src="<?php echo get_template_directory_uri();	?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
+				<div class="grid-16"><img src="<?php echo get_template_directory_uri();	?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
 			</li>
 
 		</ul>
