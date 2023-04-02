2e9185c04c1fe73b1b3e527709f0e558135d1fa7
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-04-02 11:54:36 -0300

    feat: Criação do arquivo de funções JS
    Criação do arquivo que irá levar as funções em Javascript e os resets necessários em Javascript para o Wordpress

---
+++ wp-content/themes/bikcraft/css/functions.php
@@ -0,0 +1,15 @@
+//Funções para limpar o header
+
+<?php
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'start_post_rel_link', 10, 0 );
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'adjacent_post_rel_link_wp_head', 10, 0 );
+    remove_action('wp_head', 'feed_link_extra', 3 );
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'print_emoji_detection_script', 7  );
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_scripts', 'print_emoji_styles');
+?>
