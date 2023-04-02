5d25a77875bf42ba3d0a61545f12586e7fb13626
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-04-01 23:03:48 -0300

    feat: Adição do Snipet cliente-portfolio

--- wp-content/themes/bikcraft/portfolio.php
+++ wp-content/themes/bikcraft/portfolio.php
@@ -32,25 +32,8 @@
 			</ul>
 		</section>
 
-		<section class="portfolio">
-			<div class="container">
-				<ul class="portfolio_lista rslides_portfolio">
-
-					<li>
-						<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-						<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-						<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-					</li>
-
-					<li>
-						<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-						<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-						<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-					</li>
-
-				</ul>
-			</div>
-		</section>
+		
+	<?php	include(TEMPLATEPATH . "/inc/clientes-portfolio.php");	?>
 
 	<?php	endwhile; else: endif;	?>
 
