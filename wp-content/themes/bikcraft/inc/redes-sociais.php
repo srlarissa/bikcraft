f95ffb8b7e71eac52c7df0c5add9dff11886d57b
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-04-02 20:14:46 -0300

    fix: Correção dos logos de rede social
    Adição da variável de página para que os logos no footer funcionem em qualquer página

--- wp-content/themes/bikcraft/inc/redes-sociais.php
+++ wp-content/themes/bikcraft/inc/redes-sociais.php
@@ -1,8 +1,8 @@
 <?php   $contato = get_page_by_title('contato');    ?>
 <div class="grid-4 footer_redes">
 	<ul>
-		<li><a href="<?php the_field('link_1'); ?>" target="_blank"><img src="<?php the_field('link_img_1'); ?>" alt="<?php the_field('alt_redesocial_1');   ?>"></a></li>
-		<li><a href="<?php the_field('link_2'); ?>" target="_blank"><img src="<?php the_field('link_img_2'); ?>" alt="<?php the_field('alt_redesocial_2');   ?>"></a></li>
-		<li><a href="<?php the_field('link_3'); ?>" target="_blank"><img src="<?php the_field('link_img_3'); ?>" alt="<?php the_field('alt_redesocial_3');   ?>"></a></li>
+		<li><a href="<?php the_field('link_1', $contato); ?>" target="_blank"><img src="<?php the_field('link_img_1', $contato); ?>" alt="<?php the_field('alt_redesocial_1', $contato);   ?>"></a></li>
+		<li><a href="<?php the_field('link_2', $contato); ?>" target="_blank"><img src="<?php the_field('link_img_2', $contato); ?>" alt="<?php the_field('alt_redesocial_2', $contato);   ?>"></a></li>
+		<li><a href="<?php the_field('link_3', $contato); ?>" target="_blank"><img src="<?php the_field('link_img_3', $contato); ?>" alt="<?php the_field('alt_redesocial_3', $contato);   ?>"></a></li>
     </ul>
 </div>
