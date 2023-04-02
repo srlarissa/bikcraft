6a197c99983491bac3e8f065a1a23fb0ff05b3f2
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-04-02 09:41:31 -0300

    fix: Botão sobre apenas aparece na Home

--- wp-content/themes/bikcraft/inc/qualidade.php
+++ wp-content/themes/bikcraft/inc/qualidade.php
@@ -1,22 +1,26 @@
-<section class="qualidade container">
-	<h2 class="subtitulo">Qualidade</h2>
+<?php $contato = get_page_by_title('sobre');	?>
+<section class="qualidade container"> 
+	<h2 class="subtitulo"><?php	the_field('titulo_principal', $sobre );	?></h2>
 	<img src="<?php	echo get_template_directory_uri();	?>/img/bikcraft-qualidade.png" alt="Bikcraft">
 	<ul class="qualidade_lista">
 		<li class="grid-1-3">
-			<h3><?php the_field('titulo_1');    ?></h3>
-			<p><?php the_field('content_1');    ?></p>
+			<h3><?php the_field('titulo_1', $sobre );   ?></h3>
+			<p><?php the_field('content_1', $sobre );    ?></p>
 		</li>
 		<li class="grid-1-3">
-			<h3><?php the_field('titulo_2');    ?></h3>
-			<p><?php the_field('content_2');    ?></p>
+			<h3><?php the_field('titulo_2', $sobre );    ?></h3>
+			<p><?php the_field('content_2', $sobre );    ?></p>
 		</li>
 		<li class="grid-1-3">
-			<h3><?php the_field('titulo_3');    ?></h3>
-			<p><?php the_field('content_3');    ?></p>
+			<h3><?php the_field('titulo_3', $sobre );    ?></h3>
+			<p><?php the_field('content_3', $sobre );    ?></p>
 		</li>
 	</ul>
-	<div class="call">
-		<p><?php the_field('call_sobre_texto');    ?></p>
-		<a href="/bikcraft/sobre/" class="btn btn-preto">Sobre</a>
-	</div>
+
+	<?php	if ( !is_page('sobre')) {	?>
+		<div class="call">
+			<p><?php the_field('call_sobre_texto');    ?></p>
+			<a href="/bikcraft/sobre/" class="btn btn-preto">Sobre</a>
+		</div>
+	<?php	}	?>
 </section>
