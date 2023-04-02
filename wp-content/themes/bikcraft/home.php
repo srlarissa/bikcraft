774c2d6abeb626fe7a237be128c1dae8ed5248ac
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-04-01 22:24:46 -0300

    fix: Imagem de template

--- wp-content/themes/bikcraft/home.php
+++ wp-content/themes/bikcraft/home.php
@@ -4,7 +4,16 @@
 	get_header();	
 	if ( have_posts() ) : while ( have_posts() ) : the_post();
 ?>
-	<?php	include(TEMPLATEPATH . "/inc/introducao.php");	?>
+		<section class="introducao">
+			<div class="container">
+				<h1>Bicicletas Feitas a Mão</h1>
+				<blockquote class="quote-externo">
+					<p>“não tenha nada em sua casa que você não considere útil ou acredita ser bonito”</p>
+					<cite>WILLIAM MORRIS</cite>
+				</blockquote>
+				<a href="produtos.html" class="btn">Orçamento</a>
+			</div>
+		</section>
 		
 		<section class="produtos container animar">
 			<h2 class="subtitulo">Produtos</h2>
