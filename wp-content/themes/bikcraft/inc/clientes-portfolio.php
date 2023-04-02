ece1743df08795fd6e818e54db87c0b0083fc960
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-04-01 23:01:00 -0300

    feat: Snipet Portfolio de clientes

---
+++ wp-content/themes/bikcraft/inc/clientes-portfolio.php
@@ -0,0 +1,17 @@
+<section class="portfolio">
+	<div class="container">
+		<ul class="portfolio_lista rslides_portfolio">
+			<li>
+				<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
+				<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
+				<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
+			</li>
+			<li>
+				<div class="grid-8"><img src="img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
+				<div class="grid-8"><img src="img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
+				<div class="grid-16"><img src="img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
+			</li>
+
+		</ul>
+	</div>
+</section>
