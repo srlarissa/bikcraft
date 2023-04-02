ad08a8cd076f08fb29d4c445713b4855d079d578
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-04-02 18:52:48 -0300

    feat: Criação dinâmica de citações

--- wp-content/themes/bikcraft/portfolio.php
+++ wp-content/themes/bikcraft/portfolio.php
@@ -11,22 +11,22 @@
 			<ul class="rslides">
 				<li>
 					<blockquote class="quote_clientes">
-						<p>“Pedalar sempre foi a minha paixão, o que o pessoal da Bikcraft fez foi intensificar o meu amor por esta atividade. Recomendo à todos que amo.”</p>
-						<cite>Barbara Moss</cite>
+						<?php	the_field('quote_1');	?>
+						<cite><?php	the_field('quote_author_1');	?></cite>
 					</blockquote>
 				</li>
 
 				<li>
 					<blockquote class="quote_clientes">
-						<p>“Nada melhor do que dar um rolê com a minha Bikcraft na orla. Essa é a minha companheira mais fiel, nunca me traiu e está sempre a minha disposição.”</p>
-						<cite>Jhony Rato</cite>
+						<?php	the_field('quote_2');	?>
+						<cite><?php	the_field('quote_author_2');	?></cite>
 					</blockquote>
 				</li>
 
 				<li>
 					<blockquote class="quote_clientes">
-						<p>“Aqueles que ainda não possuem uma Bikcraft, não sabem o que estão perdendo. A precisão é absurda e a velocidade transcendental. Nunca vida nada igual.”</p>
-						<cite>Bernardo</cite>
+						<?php	the_field('quote_3');	?>
+						<cite><?php	the_field('quote_author_3');	?></cite>
 					</blockquote>
 				</li>
 			</ul>
